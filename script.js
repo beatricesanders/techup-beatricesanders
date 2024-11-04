1679862acc9d2bf79b495888f7fd338a2e7f7a40
beatricesanders <54337416+beatricesanders@users.noreply.github.com>
2024-11-04 08:33:12 +0000

    #3 JS Coursework

--- script.js
+++ script.js
@@ -31,3 +31,35 @@ while (age < 70) {
     console.log("Your age is " + age );
     age = age + 1;
 }
+
+function sayHello() {
+
+    var i = 1
+
+    while (i <= 3) {
+        alert("hello everyone! x" + i)
+        i = i + 1
+    }
+   
+}
+
+
+function showInput() {
+    // Get the value from the input field of ID #user-input
+    // Store this value in a variable called user_input
+
+    var user_input = document.getElementById("user-input").value;
+
+    // Update the content for the HTML element with ID #header-age with 
+    // whatever the user_input varible contains
+
+    mood_output = document.getElementById("user-mood");
+
+    mood_output.textContent = user_input;
+
+    // check if user is sad 
+    if (user_input == "sad") {
+        mood_output.style.color = 'blue';
+    }
+}
+
